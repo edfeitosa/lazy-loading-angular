f6e462be67ce65527ea2b41e7ac45428f019a528
Eduardo Feitosa <eduardofeitosadg@gmail.com>
2021-01-24 02:30:19 -0300

    agencias component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,11 @@ import { HomeComponent } from './core/home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'agencias',
+    loadChildren: () => import('./shared/components/agencias/agencias.module').then(m => m.AgenciasModule)
+  },
 ];
 
 @NgModule({
