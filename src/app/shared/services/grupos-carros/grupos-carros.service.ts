647161012a1c6f788b9a923435a1d2594793f422
Eduardo Domingues Feitosa <eduardo.feitosa@localiza.com>
2021-02-01 17:46:24 -0300

    grupos-carros service

--- src/app/shared/services/grupos-carros/grupos-carros.service.ts
+++ src/app/shared/services/grupos-carros/grupos-carros.service.ts
@@ -4,19 +4,19 @@ import { catchError, tap, take } from "rxjs/operators";
 import { HttpClient } from "@angular/common/http";
 
 import { environment } from '../../../../environments/environment';
-import { Agencias } from '../../interfaces/agencias.interface';
+import { GruposCarros } from '../../interfaces/grupos-carros.interface';
 
 @Injectable({
   providedIn: 'any'
 })
-export class AgenciasService {
+export class GruposCarrosService {
 
   constructor(
     private http: HttpClient
   ) { }
 
-  agencias(): Observable<Agencias> {
-    return this.http.get<Agencias>(`${environment.URL_SERVICE}/agencias`)
+  agencias(): Observable<GruposCarros> {
+    return this.http.get<GruposCarros>(`${environment.URL_SERVICE}/grupos-carros`)
       .pipe(
         take(1),
         tap(sucesso => sucesso),
